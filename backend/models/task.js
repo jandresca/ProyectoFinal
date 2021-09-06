80c83faea56f523dfa47bb13cff19df3906d0826
Manuel Restrepo <manolo.1217@hotmail.com>
2021-09-06 18:49:11 -0500

    update task

--- backend/models/task.js
+++ backend/models/task.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose");
 
-const boardSchema = new mongoose.Schema({
+const taskSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.ObjectId, ref: "user" },
   panelId: { type: mongoose.Schema.ObjectId, ref: "panel" },
   name: String,
   description: String,
+  priority: Number,
   taskStatus: String,
   imageUrl: String,
+  finalDate: { type: Date, default: Date.now },
   date: { type: Date, default: Date.now },
 });
 
-const board = mongoose.model("board", boardSchema);
-module.exports = board;
+const task = mongoose.model("task", taskSchema);
+module.exports = task;
