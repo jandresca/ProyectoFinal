ff6fbcdb80e4a852e12a14f1ad30b32ce3b6d339
Juan David <juanda7473@gmail.com>
2021-09-17 17:54:42 -0500

    update

--- frontend/src/app/task/update-task/update-task.component.ts
+++ frontend/src/app/task/update-task/update-task.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskService } from '../../services/task.service';
-
+import { DatePipe } from '@angular/common';
 import { Router, ActivatedRoute } from '@angular/router';
 import {
   MatSnackBar,
@@ -10,7 +10,7 @@ import {
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-update-task',
   templateUrl: './update-task.component.html',
@@ -54,33 +54,70 @@ export class UpdateTaskComponent implements OnInit {
   uploadImg(event: any) {
     this.selectedFile = <File>event.target.files[0];
   }
+
+  transform(value: any) {
+    var datePipe = new DatePipe('en-US');
+    var fecha = new Date(value);
+    var dias = 1; // Número de días a agregar
+    value = datePipe.transform(
+      fecha.setDate(fecha.getDate() + dias),
+      'yyyy-MM-dd'
+    );
+    return value;
+  }
   updateTask() {
-    if (!this.registerData.name || !this.registerData.description) {
-      this.message = 'Failed process: Imcomplete data';
-      this.openSnackBarError();
+    if (
+      !this.registerData.name ||
+      !this.registerData.description ||
+      !this.registerData.priority ||
+      !this.registerData.finalDate
+    ) {
+      this.message = 'Failed process: Incomplete data';
+      Swal.fire({
+        allowOutsideClick: false,
+        title: 'Error!',
+        text: this.message,
+        icon: 'error',
+        confirmButtonText: 'Close',
+      });
       this.registerData = {};
     } else {
-      const data = new FormData();
-      if (this.selectedFile != null) {
-        data.append('image', this.selectedFile, this.selectedFile.name);
-      }
-      data.append('name', this.registerData.name);
-      data.append('description', this.registerData.description);
+      Swal.fire({
+        allowOutsideClick: false,
+        text: this.message,
+        icon: 'info',
+      });
+      Swal.showLoading();
 
-      this._taskService.updateTask(data).subscribe(
-        (res: any) => {
-          this._router.navigate(['/listTask']);
-          this.message = 'Task update';
-          this.openSnackBarSuccesfull();
-          this.registerData = {};
+      this._taskService.updateTaskImg(this.registerData).subscribe(
+        (res:any) => {
+          this._router.navigate(['listTask/', this.registerData.panelId]);
+          this.message = 'Successfull user registration';
+          Swal.close();
+          Swal.fire({
+            allowOutsideClick: false,
+            title: 'congratulations!',
+            text: this.message,
+            icon: 'success',
+            confirmButtonText: 'Close',
+          });
+
+        this.registerData = {};
         },
-        (err: any) => {
+        (err:any) => {
           this.message = err.error;
-          this.openSnackBarError();
+          Swal.fire({
+            allowOutsideClick: false,
+            title: 'Error!',
+            text: this.message,
+            icon: 'error',
+            confirmButtonText: 'Close',
+          });
         }
       );
     }
   }
+  
 
   openSnackBarSuccesfull() {
     this._snackBar.open(this.message, 'X', {
