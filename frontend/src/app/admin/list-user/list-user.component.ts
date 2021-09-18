8eacf4eb17135403ba0cd4d4f4c93f0032f1a8ff
Cristhian <atecristian11@gmail.com>
2021-09-17 19:00:08 -0500

    Creation alert in eliminate users

--- frontend/src/app/admin/list-user/list-user.component.ts
+++ frontend/src/app/admin/list-user/list-user.component.ts
@@ -68,7 +68,6 @@ export class ListUserComponent {
               this.userData.splice(index, 1);
               this.dataSource = new MatTableDataSource(this.userData);
               this.message = 'Delete user';
-              this.openSnackBarSuccesfull();
             }
           },
           (err) => {
@@ -90,15 +89,6 @@ export class ListUserComponent {
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
-  }
-
-  openSnackBarSuccesfull() {
-    this._snackBar.open(this.message, 'X', {
-      horizontalPosition: this.horizontalPosition,
-      verticalPosition: this.verticalPosition,
-      duration: this.durationInSeconds * 1000,
-      panelClass: ['style-snackBarTrue'],
-    });
   }
 
   openSnackBarError() {
