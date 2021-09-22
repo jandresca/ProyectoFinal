13e31acbb565058407f4c5d5037d166ac07ab366
Juan David <juanda7473@gmail.com>
2021-09-22 18:35:31 -0500

    update resumen

--- frontend/src/app/home/resumen/resumen.component.ts
+++ frontend/src/app/home/resumen/resumen.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ChartType, ChartOptions } from 'chart.js';
+import {
+  SingleDataSet,
+  Label,
+  monkeyPatchChartJsLegend,
+  monkeyPatchChartJsTooltip,
+} from 'ng2-charts';
+
 @Component({
   selector: 'app-resumen',
   templateUrl: './resumen.component.html',
-  styleUrls: ['./resumen.component.css']
+  styleUrls: ['./resumen.component.css'],
 })
 export class ResumenComponent implements OnInit {
+  public pieChartOptions: ChartOptions = {
+    responsive: true,
+  };
 
-  constructor() { }
+  public pieChartLabels: Label[] = ['PHP', '.Net', 'Java'];
+  public pieChartData: SingleDataSet = [50, 30, 20];
+  public pieChartType: ChartType = 'pie';
+  public pieChartLegend = true;
+  public pieChartPlugins = [];
 
-  ngOnInit(): void {
+  constructor() {
+    monkeyPatchChartJsTooltip();
+    monkeyPatchChartJsLegend();
   }
 
+  ngOnInit(): void {}
 }
--- frontend/src/app/task/graph/graph.component.ts
+++ frontend/src/app/task/graph/graph.component.ts
@@ -1,12 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { CargarScriptsService } from 'src/app/cargar-scripts.service';
-import { ChartType, ChartOptions } from 'chart.js';
-import {
-  SingleDataSet,
-  Label,
-  monkeyPatchChartJsLegend,
-  monkeyPatchChartJsTooltip,
-} from 'ng2-charts';
 
 @Component({
   selector: 'app-graph',
@@ -14,20 +7,9 @@ import {
   styleUrls: ['./graph.component.css'],
 })
 export class GraphComponent implements OnInit {
-  public pieChartOptions: ChartOptions = {
-    responsive: true,
-  };
-
-  public pieChartLabels: Label[] = ['PHP', '.Net', 'Java'];
-  public pieChartData: SingleDataSet = [50, 30, 20];
-  public pieChartType: ChartType = 'pie';
-  public pieChartLegend = true;
-  public pieChartPlugins = [];
-
+  
   constructor(private _CargaScripts: CargarScriptsService) {
-    monkeyPatchChartJsTooltip();
-    monkeyPatchChartJsLegend();
-    _CargaScripts.Carga(['ejemplo/ejemplo']);
+    _CargaScripts.Carga(['services/ejemplo']);
   }
 
   ngOnInit(): void {}
