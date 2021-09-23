e914ca024f2ccc0c4001b1defcee8d499c78b979
Juan David <juanda7473@gmail.com>
2021-09-22 19:50:46 -0500

    grafica

--- frontend/src/app/home/resumen/resumen.component.ts
+++ frontend/src/app/home/resumen/resumen.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { TaskService } from "../../services/task.service";
-
+import { TaskService } from '../../services/task.service';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ChartType, ChartOptions } from 'chart.js';
 import {
   SingleDataSet,
@@ -15,20 +15,57 @@ import {
   styleUrls: ['./resumen.component.css'],
 })
 export class ResumenComponent implements OnInit {
-  public pieChartOptions: ChartOptions = {
-    responsive: true,
-  };
+  todo: any = [];
+  panelId: any = '';
+  todo2:any="";
+  done:any="";
+  inprogress:any="";
 
-  public pieChartLabels: Label[] = ['PHP', '.Net', 'Java'];
-  public pieChartData: SingleDataSet = [50, 30, 20];
-  public pieChartType: ChartType = 'pie';
-  public pieChartLegend = true;
-  public pieChartPlugins = [];
-
-  constructor() {
+  constructor(
+    private _taskService: TaskService,
+    private _activatedRoute: ActivatedRoute
+  ) {
     monkeyPatchChartJsTooltip();
     monkeyPatchChartJsLegend();
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.panelId = this._activatedRoute.snapshot.paramMap.get('id');
+    console.log(this.panelId);
+    this._taskService.reporte(this.panelId).subscribe((res: any) => {
+      this.todo.push(res.done);
+      this.todo.push(res.inprogress);
+      this.todo.push(res.todo);
+
+      this.done=(res.done);
+      this.inprogress=(res.inprogress);
+      this.todo2=(res.todo);
+
+      console.log(this.todo);
+
+      console.log(this.done,this.inprogress,this.todo2);
+      
+      
+    });
+  }
+  public pieChartOptions: ChartOptions = {
+    responsive: true,
+  };
+
+  public pieChartLabels: Label[] = ['Done', 'In-Progress', 'To-Do'];
+  public pieChartData: SingleDataSet = [this.todo];
+  public pieChartType: ChartType = 'pie';
+  public pieChartLegend = true;
+  public pieChartPlugins = []; 
+  
+  public colours = ['#72C02C', '#3498DB', '#717984', '#F1C40F'];
+
+
+
+
+
 }
+
+
+
+
